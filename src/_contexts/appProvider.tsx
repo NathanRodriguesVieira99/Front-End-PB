fe13b00349da201c9dc8a3c8ab8357bb2e324b4d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-13 17:32:34 -0300

    feat: create AppProvider

---
+++ src/_contexts/appProvider.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+// componente criado para por todos os providers de contexts para o layout não ficar poluído
+
+import ReactQueryProviders from './_providers/react-query-provider';
+
+export function AppProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <ReactQueryProviders>{children}</ReactQueryProviders>;
+        </>
+    );
+}
