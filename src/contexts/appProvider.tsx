b00f273f2297bb567d96fd0b00ac5a9121252e2d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-16 14:57:57 -0300

    refactor(appProvider): fix order of 'use client' declaration

--- src/contexts/appProvider.tsx
+++ src/contexts/appProvider.tsx
@@ -1,13 +1,12 @@
-'use client';
-
 // componente criado para por todos os providers de contexts para o layout não ficar poluído
+'use client';
 
 import ReactQueryProviders from './providers/react-query-provider';
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <ReactQueryProviders>{children}</ReactQueryProviders>
-        </>
-    );
+  return (
+    <>
+      <ReactQueryProviders>{children}</ReactQueryProviders>
+    </>
+  );
 }
