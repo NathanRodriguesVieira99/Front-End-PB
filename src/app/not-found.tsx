c0ab09ea3edc1fef54247de58d1aafdb27ab25de
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-16 16:32:12 -0300

    refactor(NotFound): update 404 error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,15 @@
 'use client';
 
 export default function NotFound() {
-  return <h1>Futura página de erro 404 (página não encontrada)</h1>;
+  return (
+    <div className=" flex flex-col items-center justify-center">
+      <h1 className="mt-12 text-accent-red font-main text-heading-large">404 Not Found :(</h1>
+      <p className="text-body-medium text-accent-red-bright pt-2">اتّق دم البريء</p>
+      <p className="text-body-medium text-accent-red-bright pt-2">لا شيء مطلق، كل شيء ممكن</p>
+      <p className="text-body-medium text-accent-red-bright pt-2">اختبئ وسط الزّحام</p>
+      <p className="text-body-medium text-accent-red-bright pt-2">
+        نحن من ائتمنك، لا تخن من ائتمنك.
+      </p>
+    </div>
+  );
 }
