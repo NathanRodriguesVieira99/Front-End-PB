43791a31810f53183e059df0d28fb1d319392973
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-16 14:55:05 -0300

    refactor(login-services): enhance code comments for better clarity

--- src/_services/login-services.ts
+++ src/_services/login-services.ts
@@ -3,6 +3,7 @@
 import { prisma } from '@/lib/prisma';
 import { compare } from 'bcrypt-ts';
 
+// função para login
 export async function loginUser(data: { email: string; password: string }) {
   try {
     // busca o user no banco pelo email
@@ -10,6 +11,7 @@ export async function loginUser(data: { email: string; password: string }) {
       where: { email: data.email },
     });
 
+    // se não tem o user cadastrado da erro
     if (!user) {
       return { success: false, message: 'E-mail ou senha inválidos' };
     }
@@ -17,12 +19,15 @@ export async function loginUser(data: { email: string; password: string }) {
     // Compara as senhas para logar
     const passwordMatch = await compare(data.password, user.password);
 
+    //  dispara erro se a senha comparada não for válida
     if (!passwordMatch) {
       return { success: false, message: 'E-mail ou senha inválidos' };
     }
 
+    // autoriza o login se tudo passar
     return { success: true, user };
   } catch (error) {
+    //  pega erros inesperados
     console.error('Erro no login: ', error);
     return { success: false, message: 'Erro durante o login' };
   }
