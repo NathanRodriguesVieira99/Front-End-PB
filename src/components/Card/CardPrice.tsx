cdb022d75d0dbe4d929a2f73260a2de88ccd82eb
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-19 17:34:31 -0300

    fix(card): remove unused comments

--- src/components/Card/CardPrice.tsx
+++ src/components/Card/CardPrice.tsx
@@ -1,14 +1,13 @@
 interface CardPriceProps {
-  price: number;
+    price: number;
 }
 
-// sm:ml-[31.5rem] md:ml-[40rem] lg:ml-[55rem]
 export const CardPrice = ({ price }: CardPriceProps) => {
-  return (
-    <div className="mt-4 flex items-end justify-end">
-      <p className="text-heading-medium font-bold text-accent-green md:text-heading-medium">
-        ${price}
-      </p>
-    </div>
-  );
+    return (
+        <div className="mt-4 flex items-end justify-end">
+            <p className="text-heading-medium font-bold text-accent-green md:text-heading-medium">
+                ${price}
+            </p>
+        </div>
+    );
 };
--- src/components/Card/CardRoot.tsx
+++ src/components/Card/CardRoot.tsx
@@ -1,12 +1,11 @@
 interface CardRootProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-// sm:w-[97%] md:flex-row md:items-center
 export const CardRoot = ({ children }: CardRootProps) => {
-  return (
-    <div className="flex w-full max-w-96 flex-col flex-wrap gap-2 rounded-[5px] bg-store-darkest p-4">
-      {children}
-    </div>
-  );
+    return (
+        <div className="flex w-full max-w-96 flex-col flex-wrap gap-2 rounded-[5px] bg-store-darkest p-4">
+            {children}
+        </div>
+    );
 };
