d695a8bd6acc908edbfa453ac762509f2a61d617
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-20 17:49:19 -0300

    fix(jest): update import path for MSW setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,12 +2,12 @@
 import '@testing-library/jest-dom';
 
 // setup do MSW
-import { server } from '@/_lib/msw/setup/node';
+import { server } from '@/_api/msw/setup/node';
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 // limpa todos os mocks a cada teste
 afterEach(() => {
   jest.clearAllMocks();
 });
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
