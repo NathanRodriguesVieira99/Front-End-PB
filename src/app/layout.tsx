86be67b26769f7cab0a500521090b99e1475cd0e
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-20 17:51:31 -0300

    refactor(rootlayout): reorganize imports and update MSW setup path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 // Styles
 import './globals.css';
 
-// Fonts
-import type { Metadata } from 'next';
-import { Mona_Sans } from 'next/font/google';
-
 // metadata
-
-// Providers
-import { AppProvider } from '@/_contexts/appProvider';
-
-const monaSans = Mona_Sans({
-  subsets: ['latin'],
-});
+import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Projeto De Bloco (Front-end)',
   description: 'Front-end do nosso projeto de bloco',
@@ -20,10 +10,20 @@ export const metadata: Metadata = {
     icon: '/icon.webp',
   },
 };
+// Fonts
+import { Mona_Sans } from 'next/font/google';
+const monaSans = Mona_Sans({
+  subsets: ['latin'],
+});
+
+// Providers
+import { AppProvider } from '@/_contexts/appProvider';
 
 // MSW
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-  require('@/_lib/msw/setup');
+  if (typeof window !== 'undefined') {
+    require('@/_api/msw/setup');
+  }
 }
 
 // Root Layout
