ce8c0a9befeebe5130e54eb54897fe6fbd563f11
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-20 14:03:34 -0300

    feat(home): create base Home Page

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,7 +1,24 @@
+// import { motion } from 'motion/react';
+
+import { CarouselSize } from '@/components/Carousel/Carousel';
+
+//TODO adicionar animações com motion quando tudo estiver pronto nessa página
+/*  
+ <motion.div
+      initial={{ y: 16, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: 'easeInOut', duration: 0.75 }}
+    >
+
+*/
+
 export default function Home() {
   return (
     <>
       <h1>FUTURA HOME PAGE</h1>
+      <div className="flex items-center justify-center">
+        <CarouselSize />
+      </div>
     </>
   );
 }
