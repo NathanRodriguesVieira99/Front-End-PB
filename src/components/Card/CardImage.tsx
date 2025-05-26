cadaa9a53dee6d306b466f96940b679545c2acf6
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-26 10:07:28 -0300

    perf(cardimage): increase CardImage quality from 75 to 80

--- src/components/Card/CardImage.tsx
+++ src/components/Card/CardImage.tsx
@@ -24,7 +24,7 @@ export const CardImage = ({
         src={src}
         alt={alt}
         fill
-        quality={75}
+        quality={80}
         loading="lazy"
         sizes="(max-width: 640px) 90vw, (max-width: 768px) 45vw, (max-width: 1024px) 30vw, 379px"
         className="object-cover"
