b6b81debc434963eb3b51beaf6e50701390826b1
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-28 18:59:09 -0300

    feat(carousel): create axios fetch logic with react-query

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { IGames } from '@/@types/IGames';
 import { Card } from '@/components/Card';
 import {
   Carousel,
@@ -6,102 +11,217 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import { api } from '@/lib/axios';
+
+import { CategoryTitle } from './components/CategoryTitle';
 
 export function CarouselSize() {
-  // TODO buscar os dados dos jogos da API e exibir no map
-  // TODO fazer o CSS do Carousel
-  // TODO talvez adicionar mais lógica ao Carousel
   // Dados mockados para testes
-  const cards = [
+  const cards: IGames[] = [
     {
       id: 1,
-      alt: 'Assassin´s Creed Shadows',
-      src: '/ac_shadows.jpg',
-      title: 'Assassin´s Creed Shadows',
+      image_url: '/ac_shadows.jpg',
+      name: 'Assassin´s Creed Shadows',
       price: 49.99,
-      producedBy: 'Ubisoft',
-      releaseDate: '20/03/2025',
+      developer: 'Ubisoft',
+      release_date: '20/03/2025',
+      category: 'Releases',
     },
     {
       id: 2,
-      alt: 'Cyberpunk 2077',
-      src: '/cyberpunk.jpg',
-      title: 'Cyberpunk 2077',
+      image_url: '/cyberpunk.jpg',
+      name: 'Cyberpunk 2077',
       price: 39.99,
-      producedBy: 'CD Project Red',
-      releaseDate: '09/09/2077',
+      developer: 'CD Project Red',
+      release_date: '20/12/2020',
+      category: 'Releases',
     },
     {
       id: 3,
-      alt: 'Elden Ring',
-      src: '/elden_ring.jpg',
-      title: 'Elden Ring',
+      image_url: '/elden_ring.jpg',
+      name: 'Elden Ring',
       price: 59.99,
-      producedBy: 'Miazaki',
-      releaseDate: '22/12/2022',
+      developer: 'Miazaki',
+      release_date: '22/12/2022',
+      category: 'Releases',
     },
     {
       id: 4,
-      alt: 'God of War Ragnarök',
-      src: '/god_of_war.jpg',
-      title: 'God of War Ragnarök',
+      image_url: '/god_of_war.jpg',
+      name: 'God of War Ragnarök',
       price: 54.99,
-      producedBy: 'Santa Monica',
-      releaseDate: '03/03/2003',
+      developer: 'Santa Monica',
+      release_date: '21/12/2021',
+      category: 'Releases',
     },
     {
       id: 5,
-      alt: 'Horizon Forbidden West',
-      src: '/horizon.jpg',
-      title: 'Horizon Forbidden West',
+      image_url: '/horizon.jpg',
+      name: 'Horizon Forbidden West',
       price: 49.99,
-      producedBy: 'Sony',
-      releaseDate: '02/02/2002',
+      developer: 'Sony',
+      release_date: '19/12/2019',
+      category: 'Releases',
     },
     {
       id: 6,
-      alt: 'The Last of Us Part II',
-      src: '/tlou2.jpg',
-      title: 'The Last of Us Part II',
+      image_url: '/tlou2.jpg',
+      name: 'The Last of Us Part II',
       price: 44.99,
-      producedBy: 'Naughty Dog',
-      releaseDate: '01/01/2001',
+      developer: 'Naughty Dog',
+      release_date: '20/12/2022',
+      category: 'Releases',
     },
   ];
 
+  // LÓGICA DE FETCH dos games
+  const fetchGames = async () => {
+    try {
+      const response = await api.get<IGames[]>('API URL');
+      return response.data;
+    } catch (error) {
+      throw new Error('Erro ao buscar os jogos');
+    }
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['games'],
+    queryFn: fetchGames,
+  });
+
   return (
-    <Carousel
-      opts={{
-        align: 'start',
-        loop: true,
-        dragFree: false,
-      }}
-      className="mt-8 w-[90%]"
-    >
-      <CarouselContent className="flex w-full">
-        {cards.map((card) => (
-          <CarouselItem key={card.id} className="m-4 md:basis-1/2 lg:basis-1/3">
-            <Card.Root className="">
-              <Card.Image alt={card.alt} src={card.src} />
+    <>
+      {/* Carrousel dos jogos Lançamentos */}
 
-              <Card.Header>
-                <Card.Title title={card.title} />
-              </Card.Header>
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+          dragFree: false,
+        }}
+        className="mt-44 w-[90%]"
+      >
+        <CategoryTitle>Releases</CategoryTitle>
+        {isLoading ? (
+          <div>Carregando...</div>
+        ) : (
+          <CarouselContent className="flex w-full">
+            {cards.map((card: IGames) => (
+              <CarouselItem
+                key={card.id}
+                className="m-4 md:basis-1/2 lg:basis-1/3"
+              >
+                <Card.Root className="">
+                  <Card.Image alt={card.name} src={card.image_url} />
 
-              <Card.Content>
-                <Card.Price price={card.price} />
-              </Card.Content>
+                  <Card.Header>
+                    <Card.Title title={card.name} />
+                  </Card.Header>
 
-              <Card.Footer>
-                <Card.FooterItem text={card.producedBy} />
-                <Card.FooterItem text={card.releaseDate} />
-              </Card.Footer>
-            </Card.Root>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+                  <Card.Content>
+                    <Card.Price price={card.price} />
+                  </Card.Content>
+
+                  <Card.Footer>
+                    <Card.FooterItem text={card.developer} />
+                    <Card.FooterItem text={card.release_date} />
+                  </Card.Footer>
+                </Card.Root>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        )}
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      {/* Carrousel dos jogos Mais Vendidos */}
+
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+          dragFree: false,
+        }}
+        className="mt-32 w-[90%]"
+      >
+        <CategoryTitle>Most Sold</CategoryTitle>
+        {isLoading ? (
+          <div>Carregando...</div>
+        ) : (
+          <CarouselContent className="flex w-full">
+            {cards.map((card: IGames) => (
+              <CarouselItem
+                key={card.id}
+                className="m-4 md:basis-1/2 lg:basis-1/3"
+              >
+                <Card.Root className="">
+                  <Card.Image alt={card.name} src={card.image_url} />
+
+                  <Card.Header>
+                    <Card.Title title={card.name} />
+                  </Card.Header>
+
+                  <Card.Content>
+                    <Card.Price price={card.price} />
+                  </Card.Content>
+
+                  <Card.Footer>
+                    <Card.FooterItem text={card.developer} />
+                    <Card.FooterItem text={card.release_date} />
+                  </Card.Footer>
+                </Card.Root>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        )}
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      {/* Carrousel dos jogos Mais Jogados */}
+
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+          dragFree: false,
+        }}
+        className="mt-32 mb-12 w-[90%]"
+      >
+        <CategoryTitle>Most Played</CategoryTitle>
+        {isLoading ? (
+          <div>Carregando...</div>
+        ) : (
+          <CarouselContent className="flex w-full">
+            {cards.map((card: IGames) => (
+              <CarouselItem
+                key={card.id}
+                className="m-4 md:basis-1/2 lg:basis-1/3"
+              >
+                <Card.Root className="">
+                  <Card.Image alt={card.name} src={card.image_url} />
+
+                  <Card.Header>
+                    <Card.Title title={card.name} />
+                  </Card.Header>
+
+                  <Card.Content>
+                    <Card.Price price={card.price} />
+                  </Card.Content>
+
+                  <Card.Footer>
+                    <Card.FooterItem text={card.developer} />
+                    <Card.FooterItem text={card.release_date} />
+                  </Card.Footer>
+                </Card.Root>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        )}
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
   );
 }
