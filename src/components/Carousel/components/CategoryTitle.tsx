65074abcd5ec7e419456476591b6443f6743b393
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-28 18:48:08 -0300

    feat(category-title): create CategoryTitle component

---
+++ src/components/Carousel/components/CategoryTitle.tsx
@@ -0,0 +1,24 @@
+import type { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface CategoryTitleProps extends ComponentProps<'h2'> {
+  children: React.ReactNode;
+}
+
+export const CategoryTitle = ({
+  children,
+  className,
+  ...props
+}: CategoryTitleProps) => {
+  return (
+    <h2
+      {...props}
+      className={twMerge(
+        'justify-start text-lg font-bold text-text-main',
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+};
