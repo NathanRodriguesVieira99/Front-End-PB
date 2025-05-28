1eaded6c5a5141671dce1631d241cda23f0ba979
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-28 18:55:11 -0300

    feat(home page): add motion and suspense loading

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,22 +1,31 @@
-// import { motion } from 'motion/react';
+'use client';
+
+import { motion } from 'motion/react';
+import { Suspense } from 'react';
 
 import { CarouselSize } from '@/components/Carousel';
+import { Hero } from '@/components/layout/Hero';
 
-//TODO adicionar animações com motion quando tudo estiver pronto nessa página
-/*  
- <motion.div
-      initial={{ y: 16, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 0.75 }}
-    >
-
-*/
+// TODO Card diferente entre a imagem do topo e os carrousels normais
 
 export default function HomePage() {
   return (
-    <main className="h-screen bg-[linear-gradient(180deg,var(--color-store-dark)_0%,var(--color-store-darker)_80%)]">
+    <main className="min-h-screen bg-gradient-to-b from-gray-950 via-slate-900 to-slate-800">
       <div className="flex flex-col items-center justify-center">
-        <CarouselSize />
+        <Suspense
+          fallback={
+            <div className="text-2xl text-text-main">Carregando...</div>
+          }
+        >
+          <motion.div
+            initial={{ y: 16, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ ease: 'easeInOut', duration: 0.75 }}
+          >
+            <Hero />
+          </motion.div>
+          <CarouselSize />
+        </Suspense>
       </div>
     </main>
   );
