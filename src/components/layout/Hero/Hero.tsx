74ac749136171504e9dff7cf9ab96a2b78dc57bb
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-28 18:52:53 -0300

    feat(hero): create Hero image component

--- src/components/layout/Hero/Hero.tsx
+++ src/components/layout/Hero/Hero.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+
 export const Hero = () => {
-  return <></>;
+  return (
+    <div className="">
+      <Image
+        src="/images/Hero_img.gif"
+        alt="Imagem de fundo"
+        layout={'responsive'}
+        height={623}
+        width={1920}
+        unoptimized={true}
+      />
+    </div>
+  );
 };
