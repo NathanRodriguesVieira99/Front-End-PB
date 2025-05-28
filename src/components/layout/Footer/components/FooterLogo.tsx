f20d8be922b76a8c324ec964ccede4f7a6fde93e
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-28 18:51:21 -0300

    feat(footer-logo): create FooterLogo, add mock logo image and motion

--- src/components/layout/Footer/components/FooterLogo.tsx
+++ src/components/layout/Footer/components/FooterLogo.tsx
@@ -1,9 +1,40 @@
-export const Footerlogo = () => {
+'use client';
+
+import { motion } from 'motion/react';
+import Image from 'next/image';
+import type { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface FooterLogoProps extends ComponentProps<typeof motion.div> {
+  src: string;
+  alt: string;
+  height: number;
+  width: number;
+}
+
+export const Footerlogo = ({
+  src,
+  alt,
+  height,
+  width,
+  className,
+}: FooterLogoProps) => {
   return (
-    <div className="">
-      <h1 className="mt-8 text-text-main">FUTURA LOGO</h1>
-    </div>
+    <motion.div
+      className=""
+      initial={{ opacity: 0, x: -300 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -300 }}
+      transition={{ duration: 0.5, delay: 0.4 }}
+      viewport={{ once: true }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        className={twMerge('', className)}
+        height={height}
+        width={width}
+      />
+    </motion.div>
   );
 };
-
-// TODO transformar em imagem, estilizar e adicionar twMerge
