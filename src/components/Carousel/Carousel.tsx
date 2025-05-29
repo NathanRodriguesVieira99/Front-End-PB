c8b5197b23731413ec062058055edc712f30f8a5
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-29 14:16:29 -0300

    feat(carousel): add filter method to dinamic display games by category

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -33,7 +33,7 @@ export function CarouselSize() {
 
   return (
     <>
-      {/* Carrousel dos jogos Lançamentos */}
+      {/* Carrousel dos jogos FPS */}
 
       <Carousel
         opts={{
@@ -43,41 +43,46 @@ export function CarouselSize() {
         }}
         className="mt-44 w-[90%]"
       >
-        <CategoryTitle>Releases</CategoryTitle>
+        <CategoryTitle>FPS</CategoryTitle>
         {isLoading ? (
           <div>Carregando...</div>
         ) : (
           <CarouselContent className="flex w-full">
-            {data?.map((game_data: IGames) => (
-              <CarouselItem
-                key={game_data.id}
-                className="m-4 md:basis-1/2 lg:basis-1/3"
-              >
-                <Card.Root className="">
-                  <Card.Image alt={game_data.name} src={game_data.image_url} />
+            {data
+              ?.filter((game_data: IGames) => game_data.category === 'FPS')
+              .map((game_data: IGames) => (
+                <CarouselItem
+                  key={game_data.id}
+                  className="m-4 md:basis-1/2 lg:basis-1/3"
+                >
+                  <Card.Root className="">
+                    <Card.Image
+                      alt={game_data.name}
+                      src={game_data.image_url}
+                    />
 
-                  <Card.Header>
-                    <Card.Title title={game_data.name} />
-                  </Card.Header>
+                    <Card.Header>
+                      <Card.Title title={game_data.name} />
+                    </Card.Header>
 
-                  <Card.Content>
-                    <Card.Price price={game_data.price} />
-                  </Card.Content>
+                    <Card.Content>
+                      <Card.Price price={game_data.price} />
+                    </Card.Content>
 
-                  <Card.Footer>
-                    <Card.FooterItem text={game_data.developer} />
-                    <Card.FooterItem text={game_data.release_date} />
-                  </Card.Footer>
-                </Card.Root>
-              </CarouselItem>
-            ))}
+                    <Card.Footer>
+                      <Card.FooterItem text={game_data.developer} />
+                      <Card.FooterItem text={game_data.release_date} />
+                    </Card.Footer>
+                  </Card.Root>
+                </CarouselItem>
+              ))}
           </CarouselContent>
         )}
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
 
-      {/* Carrousel dos jogos Mais Vendidos */}
+      {/* Carrousel dos jogos de mundo aberto */}
 
       <Carousel
         opts={{
@@ -87,41 +92,48 @@ export function CarouselSize() {
         }}
         className="mt-32 w-[90%]"
       >
-        <CategoryTitle>Most Sold</CategoryTitle>
+        <CategoryTitle>Open World</CategoryTitle>
         {isLoading ? (
           <div>Carregando...</div>
         ) : (
           <CarouselContent className="flex w-full">
-            {data?.map((game_data: IGames) => (
-              <CarouselItem
-                key={game_data.id}
-                className="m-4 md:basis-1/2 lg:basis-1/3"
-              >
-                <Card.Root className="">
-                  <Card.Image alt={game_data.name} src={game_data.image_url} />
+            {data
+              ?.filter(
+                (game_data: IGames) => game_data.category === 'Open World'
+              )
+              .map((game_data: IGames) => (
+                <CarouselItem
+                  key={game_data.id}
+                  className="m-4 md:basis-1/2 lg:basis-1/3"
+                >
+                  <Card.Root className="">
+                    <Card.Image
+                      alt={game_data.name}
+                      src={game_data.image_url}
+                    />
 
-                  <Card.Header>
-                    <Card.Title title={game_data.name} />
-                  </Card.Header>
+                    <Card.Header>
+                      <Card.Title title={game_data.name} />
+                    </Card.Header>
 
-                  <Card.Content>
-                    <Card.Price price={game_data.price} />
-                  </Card.Content>
+                    <Card.Content>
+                      <Card.Price price={game_data.price} />
+                    </Card.Content>
 
-                  <Card.Footer>
-                    <Card.FooterItem text={game_data.developer} />
-                    <Card.FooterItem text={game_data.release_date} />
-                  </Card.Footer>
-                </Card.Root>
-              </CarouselItem>
-            ))}
+                    <Card.Footer>
+                      <Card.FooterItem text={game_data.developer} />
+                      <Card.FooterItem text={game_data.release_date} />
+                    </Card.Footer>
+                  </Card.Root>
+                </CarouselItem>
+              ))}
           </CarouselContent>
         )}
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
 
-      {/* Carrousel dos jogos Mais Jogados */}
+      {/* Carrousel dos jogos RPG */}
 
       <Carousel
         opts={{
@@ -131,34 +143,39 @@ export function CarouselSize() {
         }}
         className="mt-32 mb-12 w-[90%]"
       >
-        <CategoryTitle>Most Played</CategoryTitle>
+        <CategoryTitle>RPG</CategoryTitle>
         {isLoading ? (
           <div>Carregando...</div>
         ) : (
           <CarouselContent className="flex w-full">
-            {data?.map((game_data: IGames) => (
-              <CarouselItem
-                key={game_data.id}
-                className="m-4 md:basis-1/2 lg:basis-1/3"
-              >
-                <Card.Root className="">
-                  <Card.Image alt={game_data.name} src={game_data.image_url} />
+            {data
+              ?.filter((game_data: IGames) => game_data.category === 'RPG')
+              .map((game_data: IGames) => (
+                <CarouselItem
+                  key={game_data.id}
+                  className="m-4 md:basis-1/2 lg:basis-1/3"
+                >
+                  <Card.Root className="">
+                    <Card.Image
+                      alt={game_data.name}
+                      src={game_data.image_url}
+                    />
 
-                  <Card.Header>
-                    <Card.Title title={game_data.name} />
-                  </Card.Header>
+                    <Card.Header>
+                      <Card.Title title={game_data.name} />
+                    </Card.Header>
 
-                  <Card.Content>
-                    <Card.Price price={game_data.price} />
-                  </Card.Content>
+                    <Card.Content>
+                      <Card.Price price={game_data.price} />
+                    </Card.Content>
 
-                  <Card.Footer>
-                    <Card.FooterItem text={game_data.developer} />
-                    <Card.FooterItem text={game_data.release_date} />
-                  </Card.Footer>
-                </Card.Root>
-              </CarouselItem>
-            ))}
+                    <Card.Footer>
+                      <Card.FooterItem text={game_data.developer} />
+                      <Card.FooterItem text={game_data.release_date} />
+                    </Card.Footer>
+                  </Card.Root>
+                </CarouselItem>
+              ))}
           </CarouselContent>
         )}
         <CarouselPrevious />
