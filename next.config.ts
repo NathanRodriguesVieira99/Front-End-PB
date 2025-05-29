036d46d955676e44e8d1be80baa001bd1772f977
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-29 12:35:51 -0300

    feat(next-config): add remotePatterns for image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn2.steamgriddb.com',
+        port: '',
+        pathname: '/thumb/**',
+      },
+    ],
   },
   transpilePackages: ['msw'],
 };
