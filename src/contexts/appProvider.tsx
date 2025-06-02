d7fe3a1e924feddee64fdf74158edbd60a9fd20c
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-02 19:39:11 -0300

    refactor(app provider): remove MSWProvider

--- src/contexts/appProvider.tsx
+++ src/contexts/appProvider.tsx
@@ -1,14 +1,8 @@
 // componente criado para por todos os providers de contexts para o layout não ficar poluído
 'use client';
 
-import { MSWProvider } from './providers/msw-provider';
 import ReactQueryProviders from './providers/react-query-provider';
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <MSWProvider />
-      <ReactQueryProviders>{children}</ReactQueryProviders>
-    </>
-  );
+  return <ReactQueryProviders>{children}</ReactQueryProviders>;
 }
