dee68f75064adde43dfc18ff42eb78fd5535c671
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-02 17:35:58 -0300

    feat(msw mocks): create base handlers

--- src/api/msw/mocks/index.ts
+++ src/api/msw/mocks/index.ts
@@ -2,7 +2,20 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
+  http.post('', () => {
+    return HttpResponse.json(
+      {},
+      {
+        status: 200,
+      }
+    );
+  }),
   http.get('', () => {
-    return HttpResponse.json({}, { status: 200 });
+    return HttpResponse.json(
+      {},
+      {
+        status: 200,
+      }
+    );
   }),
 ];
