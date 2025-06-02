85b80cc9dba21972eca623adc61041584e7733df
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-02 17:20:55 -0300

    feat(msw): add conditional initialization for MSW in browser, development and test environments

--- src/api/msw/setup/browser.ts
+++ src/api/msw/setup/browser.ts
@@ -3,3 +3,10 @@ import { setupWorker } from 'msw/browser';
 import { handlers } from '../mocks';
 
 export const worker = setupWorker(...handlers);
+
+// Inicializa o MSW apenas no browser e em ambiente de desenvolvimento ou teste
+if (
+  typeof window !== 'undefined' &&
+  (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
+) {
+}
