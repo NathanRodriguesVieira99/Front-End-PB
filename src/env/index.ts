6dd2debef0b05205128d8dff2a7b1bcd21ed1204
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-02 17:34:25 -0300

    chore(env): add TODO comment for environment variables validation usage

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,3 +11,5 @@ export const env = envSchema.parse({
   NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
   DATABASE_URL: process.env.DATABASE_URL,
 });
+
+// TODO adicionar essas validações onde chama essas variáveis de ambiente
