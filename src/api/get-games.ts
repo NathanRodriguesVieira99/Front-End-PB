717efc61904268943a33e1f871e9c7355552c38b
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-03 21:06:17 -0300

    refactor(api): transform fetchGames function in a custom hook with useCallback

--- src/api/get-games.ts
+++ src/api/get-games.ts
@@ -1,12 +1,20 @@
+import { useCallback } from 'react';
+
 import type { IGames } from '@/@types/IGames';
 import { api } from '@/lib/axios';
 
 // LÓGICA DE FETCH dos games
-export const fetchGames = async () => {
-  try {
-    const response = await api.get<IGames[]>('game');
-    return response.data;
-  } catch (error) {
-    throw new Error('Erro ao buscar os jogos');
-  }
+/*
+Custom hook para fazer fetch com o axios
+*/
+export const useFetchGames = () => {
+  const fetchGames = useCallback(async (): Promise<IGames[]> => {
+    try {
+      const response = await api.get<IGames[]>('game');
+      return response.data;
+    } catch (error) {
+      throw new Error('Erro ao buscar os jogos');
+    }
+  }, []);
+  return fetchGames;
 };
